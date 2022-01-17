aadc3d3a8effeda1b4d72736255792f13c1eda0e
Arthur Valladares <53414712+vnaj16@users.noreply.github.com>
2022-01-16 19:21:02 -0500

    Initial commit

---
+++ Demo_Benchmarking_NET/MethodsToCompare.cs
@@ -0,0 +1,61 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Benchmarking_NET
+{
+    [MemoryDiagnoser]
+    public  class MethodsToCompare
+    {
+        //STRING BUILDER
+        private string[] words = { "Hola", "Soy", "Arthur", "Javier", "Valladares", "Nole", "Hola", "Soy", "Arthur", "Javier", "Valladares", "Nole", "Hola", "Soy", "Arthur", "Javier", "Valladares", "Nole", };
+        private int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 1, 2, 3, 4, 5, 6, 7, 146, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, };
+
+        [Benchmark(Baseline = true)]
+        public string BuildStringNormal()
+        {
+            string finalWord = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                finalWord += words[i] + " ";
+            }
+            return finalWord;
+        }
+
+        [Benchmark]
+        public string BuildStringWithBuilder()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < words.Length; i++)
+            {
+                stringBuilder.Append(words[i]);
+                stringBuilder.Append(" ");
+            }
+            return stringBuilder.ToString();
+
+        }
+        //Find and Element: Where vs For
+        //[Benchmark]
+        public bool IsPresent146WithFor()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]==146)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //[Benchmark]
+        public bool IsPresent146WithLINQ()
+        {
+
+            return numbers.Contains(146);
+        }
+    }
+}
---
+++ Demo_Benchmarking_NET/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using Demo_Benchmarking_NET;
+
+var results = BenchmarkRunner.Run<MethodsToCompare>();
+//Console.WriteLine(results);
+Console.ReadKey();
